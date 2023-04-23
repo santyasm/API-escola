4147f374e56eaadf114a0f3fe46b0e701edd0ac5
Yasmin Santana <94482549+santyasm@users.noreply.github.com>
2023-04-23 18:54:16 -0300

    Endpoints de alunos criados

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -10,4 +10,56 @@ export default class AlunoController {
 			return res.status(400).json({errors: [error.message]});
 		}
 	}
+
+	static async index(req, res) {
+		try {
+			const alunos = await Aluno.findAll({ order: [['id', 'desc']] });
+			
+			return res.json(alunos);
+		} catch (error) {
+			return res.status(400).json({ errors: [error.message] });
+		}
+	}
+
+	static async show(req, res) {
+		try {
+			const { id } = req.params;
+			const aluno = await Aluno.findByPk(id);
+			if (!aluno) return res.status(401).json({ errors: ['Aluno não existe'] });
+			
+			return res.json(aluno);
+		} catch (error) {
+			return res.status(400).json({ errors: [error.message] });
+		}
+	}
+
+	static async update(req, res) {
+		try {
+			const { id } = req.params;
+			const aluno = await Aluno.findByPk(id);
+			if (!aluno) return res.status(401).json({ errors: ['Aluno não existe'] });
+
+			const alunoUpdated = await aluno.update(req.body);
+
+			return res.json(alunoUpdated);
+		} catch (error) {
+			return res.status(400).json({ errors: [error.message] });
+		}
+
+	}
+
+	static async delete(req, res) {
+		try {
+			const { id } = req.params;
+			const aluno = await Aluno.findByPk(id);
+			if (!aluno) return res.status(401).json({ errors: ['Aluno não existe'] });
+			
+			await aluno.destroy();
+
+			return res.status(204).json({});
+		} catch (error) {
+			return res.status(400).json({ errors: [error.message] });
+		}
+	}
+ 
 }
--- src/routes/alunoRoute.js
+++ src/routes/alunoRoute.js
@@ -3,5 +3,9 @@ import AlunoController from '../controllers/AlunoController';
 const router = Router();
 
 router.post('/aluno', AlunoController.store);
+router.get('/alunos', AlunoController.index);
+router.get('/aluno/:id', AlunoController.show);
+router.delete('/aluno/:id', AlunoController.delete);
+router.put('/aluno/:id', AlunoController.update);
 
 export default router;
