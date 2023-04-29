929793f7d712ce6c1f33f0b2c5b419d7b88d33f3
Yasmin Santana <94482549+santyasm@users.noreply.github.com>
2023-04-29 11:54:42 -0300

    Salvando foto com id do aluno

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -3,15 +3,28 @@ import multer from 'multer';
 import multerConfig from '../config/multer';
 const upload = multer(multerConfig).single('foto');
 
-export default class FotoController{
+export default class FotoController {
 	static store(req, res) {
 		return upload(req, res, async (error) => {
-			if (error) return res.status(400).json({
-				errors: [error.code]
-			});
-			const { filename, originalname } = req.file;
-			const newFoto = await Foto.create({filename, originalname});
-			res.json(newFoto);
+			if (error)
+				return res.status(400).json({
+					errors: [error.code],
+				});
+
+			try {
+				const { filename, originalname } = req.file;
+				const { aluno_id } = req.body;
+
+				const newFoto = await Foto.create({
+					filename,
+					originalname,
+					aluno_id,
+				});
+				res.json(newFoto);
+			} catch (error) {
+				return res.status(400).json({errors: ['Aluno não existe.']});
+			}
 		});
 	}
 }
+
